57f042b9c718cedfec5a01e5de960b885e5d8acd
Maxim Loginov <zeliboba@mail.ru>
2011-07-08 21:14:49 +0200

    initial working commit

---
+++ load.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener("load", function() {
+  var entry = document.querySelectorAll('div.hentry');
+  var title = entry[0].querySelector('.single-entry-title');
+  var bar = entry[0].querySelector('.entry-info');
+  title.parentNode.insertBefore(bar, title);
+  title.style.clear = "both";
+}, false);
